f7e9ffed1e246e40c2a07693729952512fd1018a
Alex Pridorozhny <obliterator8814@gmail.com>
2015-11-29 22:26:31 +0300

    Class and session for groups created

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,11 +11,11 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="666", header="666", footer="666"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="666", header="666", footer="666"))
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", header="", footer=""))
+    app.session.logout()
