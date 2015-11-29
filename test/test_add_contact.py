63753976fef3fadec21b23e0b3ddeda597d68e56
Alex Pridorozhny <obliterator8814@gmail.com>
2015-11-30 00:05:08 +0300

    Class and sessions for contacts created fixed

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,12 +11,12 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.open_new_contact()
+    app.contact.open_new_contact()
     app.contact.create(Contact(name="oleg", middle="lol", home="321654", mobile="321654", phone="qwe123"))
     app.session.logout()
 
 def test_add_emptycontact(app):
     app.session.login(username="admin", password="secret")
-    app.open_new_contact()
+    app.contact.open_new_contact()
     app.contact.create(Contact(name="", middle="", home="", mobile="", phone=""))
     app.session.logout()
