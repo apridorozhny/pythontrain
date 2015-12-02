112a4967bedb93a2e729ee6991118dd333de6e6d
Alex Pridorozhny <obliterator8814@gmail.com>
2015-12-03 00:47:55 +0300

    Modifications and deletions of groups and contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -27,6 +27,36 @@ class ContactHelper:
         # submit new contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
+
+    def mod_first_contact(self, contact):
+        wd = self.app.wd
+        # submit new contact creation
+        wd.find_element_by_xpath("//div/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
+        # init and fill contact
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.phone)
+        wd.find_element_by_name("update").click()
+
     def open_new_contact(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
